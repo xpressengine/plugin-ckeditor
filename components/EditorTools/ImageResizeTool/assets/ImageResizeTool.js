83b24e2ed1b1f73dca8824bfa41664674a5858d2
bnu <bnufactory@gmail.com>
2018-03-13 16:16:16 +0900

    js 파일 포맷 정리

--- components/EditorTools/ImageResizeTool/assets/ImageResizeTool.js
+++ components/EditorTools/ImageResizeTool/assets/ImageResizeTool.js
@@ -1,51 +1,51 @@
 XEeditor.tools.define({
-	id : 'editortool/image_resize_tool@image_resize_crop',
-	events: {
-		iconClick: function(targetEditor, cbAppendToolContent) {
-			if(targetEditor.config.perms.upload) {
-				var $uploadArea = $(targetEditor.container.$).parent().find('.ckeditor-fileupload-area');
-				// var currentFilesSize = parseFloat($uploadArea.find('.currentFilesSize').text());
+  id: 'editortool/image_resize_tool@image_resize_crop',
+  events: {
+    iconClick: function (targetEditor, cbAppendToolContent) {
+      if (targetEditor.config.perms.upload) {
+        var $uploadArea = $(targetEditor.container.$).parent().find('.ckeditor-fileupload-area')
+        // var currentFilesSize = parseFloat($uploadArea.find('.currentFilesSize').text());
 
-				var uploadUrl = targetEditor.config.fileUpload.upload_url;
-				var attachMaxSize = targetEditor.config.attachMaxSize;
-				var fileMaxSize = targetEditor.config.fileMaxSize;
-				var extensions = targetEditor.config.extensions;
+        var uploadUrl = targetEditor.config.fileUpload.upload_url
+        var attachMaxSize = targetEditor.config.attachMaxSize
+        var fileMaxSize = targetEditor.config.fileMaxSize
+        var extensions = targetEditor.config.extensions
 
-				var cWindow = window.open(imageResizeURL.get('popup'), 'createImageResizePopup', "width=850,height=970,directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no");
+        var cWindow = window.open(imageResizeURL.get('popup'), 'createImageResizePopup', 'width=850,height=970,directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no')
 
-				$(cWindow).on('load', function() {
-					cWindow.targetEditor = targetEditor;
-					cWindow.appendToolContent = cbAppendToolContent;
-					cWindow.uploadInfo = {
-						uploadUrl: uploadUrl,
-						attachMaxSize: attachMaxSize,
-						fileMaxSize: fileMaxSize,
-						extensions: extensions,
-						$uploadArea: $uploadArea
-					};
-				});
-			}
-		},
-		elementDoubleClick: function() {
+        $(cWindow).on('load', function () {
+          cWindow.targetEditor = targetEditor
+          cWindow.appendToolContent = cbAppendToolContent
+          cWindow.uploadInfo = {
+            uploadUrl: uploadUrl,
+            attachMaxSize: attachMaxSize,
+            fileMaxSize: fileMaxSize,
+            extensions: extensions,
+            $uploadArea: $uploadArea
+          }
+        })
+      }
+    },
+    elementDoubleClick: function () {
 
-		},
-		beforeSubmit: function(targetEditor) {
+    },
+    beforeSubmit: function (targetEditor) {
 
-		},
-		editorLoaded: function(targetEditor) {
-			if(!targetEditor.config.perms.upload) {
-				$('.cke_button__imageresizer').addClass('cke_button_disabled');
-			}
-		}
-	},
-	props: {
-		name: 'ImageResizeTool',
-		options: {
-			label: '이미지 편집',
-			command: 'openImageResizeTool'
-		},
-		addEvent: {
-			doubleClick: false
-		}
-	}
-});
+    },
+    editorLoaded: function (targetEditor) {
+      if (!targetEditor.config.perms.upload) {
+        $('.cke_button__imageresizer').addClass('cke_button_disabled')
+      }
+    }
+  },
+  props: {
+    name: 'ImageResizeTool',
+    options: {
+      label: '이미지 편집',
+      command: 'openImageResizeTool'
+    },
+    addEvent: {
+      doubleClick: false
+    }
+  }
+})
