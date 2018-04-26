d37e394478eadae6db6e40549499f6b6a44219d0
bnu <bnufactory@gmail.com>
2018-04-26 18:40:52 +0900

    코드 정리 - 커스텀 플러그인. CKEditor 라이브러리와 분리 필요함

--- assets/ckeditor/plugins/touchToolbar/plugin.js
+++ assets/ckeditor/plugins/touchToolbar/plugin.js
@@ -1,65 +1,64 @@
+// @FIXME 이동
 CKEDITOR.plugins.add('touchToolbar', {
-	init: function ( editor ) {
-		var onlongtouch;
-		var timer, lockTimer;
-		var touchduration = 800;
-		var touches = '';
+  init: function (editor) {
+    var $ = window.jQuery
+    var onlongtouch
+    var timer, lockTimer
+    var touchduration = 800
+    var touches = ''
 
-		function touchstart(e) {
-      if(lockTimer){
-        return;
+    function touchstart (e) {
+      if (lockTimer) {
+        return
       }
-      touches = e.originalEvent.touches[0];
-      timer = setTimeout(onlongtouch, touchduration);
-      lockTimer = true;
-      editor.focus();
-		}
+      touches = e.originalEvent.touches[0]
+      timer = setTimeout(onlongtouch, touchduration)
+      lockTimer = true
+      editor.focus()
+    }
 
-		function touchend() {
-			if (timer){
-				clearTimeout(timer);
-				touches = '';
-				lockTimer = false;
-			}
-		}
+    function touchend () {
+      if (timer) {
+        clearTimeout(timer)
+        touches = ''
+        lockTimer = false
+      }
+    }
 
-		onlongtouch = function() {
+    onlongtouch = function () {
+      if (touches) {
+        var content = document.getElementsByClassName('cke_contents').item(0)
+        var toolbar = document.getElementsByClassName('cke_top').item(0)
+        var cke_editor = document.getElementsByClassName('cke').item(0)
 
-			if(touches) {
-				var content = document.getElementsByClassName('cke_contents').item(0);
-				var toolbar = document.getElementsByClassName('cke_top').item(0);
-				var cke_editor = document.getElementsByClassName('cke').item(0);
+        $(toolbar).fadeOut('fast', function () {
+          cke_editor.style.position = 'relative'
+          toolbar.style.borderTop = '1px solid #ddd'
+          toolbar.style.width = content.offsetWidth + 'px'
+          toolbar.style.top = (touches.clientY - 20) + 'px'
+          toolbar.style.left = '0px'
+          toolbar.style.right = '0px'
+          toolbar.style.margin = '0 auto'
+          toolbar.style.boxSizing = 'border-box'
+          toolbar.style.zIndex = '1000'
+          toolbar.style.position = 'absolute'
 
-        $(toolbar).fadeOut('fast', function() {
-          cke_editor.style.position = 'relative';
-          toolbar.style.borderTop = "1px solid #ddd";
-          toolbar.style.width = content.offsetWidth + "px";
-          toolbar.style.top = (touches.clientY - 20) + "px";
-          toolbar.style.left = "0px";
-          toolbar.style.right = "0px";
-          toolbar.style.margin = "0 auto";
-          toolbar.style.boxSizing = "border-box";
-          toolbar.style.zIndex = "1000";
-          toolbar.style.position = "absolute";
+          $(this).fadeIn('fast')
+        })
+      }
+    }
 
-          $(this).fadeIn('fast');
+    resize = function () {
+      var content = document.getElementsByClassName('cke_contents').item(0)
+      var toolbar = document.getElementsByClassName('cke_top').item(0)
 
-        });
+      toolbar.style.width = content.offsetWidth + 'px'
+    }
 
-			}
-		};
-
-		resize = function() {
-			var content = document.getElementsByClassName('cke_contents').item(0);
-			var toolbar = document.getElementsByClassName('cke_top').item(0);
-
-			toolbar.style.width = content.offsetWidth + "px";
-		};
-
-		editor.on("instanceReady", function(event) {
-			$(event.editor.document.$).on('touchstart', touchstart);
-			$(event.editor.document.$).on('touchend', touchend);
-			$(window).on('resize', resize);
-		});
-	}
-});
+    editor.on('instanceReady', function (event) {
+      $(event.editor.document.$).on('touchstart', touchstart)
+      $(event.editor.document.$).on('touchend', touchend)
+      $(window).on('resize', resize)
+    })
+  }
+})
--- assets/ckeditor/plugins/xeFixed/plugin.js
+++ assets/ckeditor/plugins/xeFixed/plugin.js
@@ -1,35 +1,35 @@
-CKEDITOR.plugins.add( 'xeFixed', {
-    init: function( editor ) {
-        function handleToolbar() {
-            var content = document.getElementsByClassName('cke_contents').item(0);
-            var toolbar = document.getElementsByClassName('cke_top').item(0);
-            var editor = document.getElementsByClassName('cke').item(0);
-            var scrollvalue = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+// @FIXME 이동
+CKEDITOR.plugins.add('xeFixed', {
+  init: function (editor) {
+    function handleToolbar () {
+      var $ = window.jQuery
+      var content = document.getElementsByClassName('cke_contents').item(0)
+      var toolbar = document.getElementsByClassName('cke_top').item(0)
+      var editor = document.getElementsByClassName('cke').item(0)
+      var scrollvalue = document.documentElement.scrollTop > document.body.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop
 
-            if($(editor).offset().top < scrollvalue
-              && ($(editor).offset().top + $(editor).height() - $(toolbar).height()) > scrollvalue ) {
-                toolbar.style.width = content.offsetWidth + "px";
-                toolbar.style.top = "0px";
-                toolbar.style.left = "0px";
-                toolbar.style.right = "0px";
-                toolbar.style.margin = "0 auto";
-                toolbar.style.boxSizing = "border-box";
-                toolbar.style.zIndex = "1000";
-                toolbar.style.position = "fixed";
-
-            } else {
-                toolbar.style.removeProperty("position");
-                toolbar.style.removeProperty("zIndex");
-                toolbar.style.removeProperty("top");
-                toolbar.style.removeProperty("left");
-                toolbar.style.removeProperty("right");
-                toolbar.style.removeProperty("margin");
-                toolbar.style.removeProperty("boxSizing");
-                toolbar.style.removeProperty("width");
-
-            }
-        }
-
-        $(window).on('scroll resize', handleToolbar);
+      if ($(editor).offset().top < scrollvalue &&
+              ($(editor).offset().top + $(editor).height() - $(toolbar).height()) > scrollvalue) {
+        toolbar.style.width = content.offsetWidth + 'px'
+        toolbar.style.top = '0px'
+        toolbar.style.left = '0px'
+        toolbar.style.right = '0px'
+        toolbar.style.margin = '0 auto'
+        toolbar.style.boxSizing = 'border-box'
+        toolbar.style.zIndex = '1000'
+        toolbar.style.position = 'fixed'
+      } else {
+        toolbar.style.removeProperty('position')
+        toolbar.style.removeProperty('zIndex')
+        toolbar.style.removeProperty('top')
+        toolbar.style.removeProperty('left')
+        toolbar.style.removeProperty('right')
+        toolbar.style.removeProperty('margin')
+        toolbar.style.removeProperty('boxSizing')
+        toolbar.style.removeProperty('width')
+      }
     }
-});
+
+    window.jQuery(window).on('scroll resize', handleToolbar)
+  }
+})
